8f3686592bbb67538f69e193909397d66f450abb
tamaprogressgroup <138646983+tamaprogressgroup@users.noreply.github.com>
2025-10-20 22:07:18 +0700

    ref comment

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 
-// 👇 PASTIKAN BARIS INI ADA DAN DI ATAS IMPORT CSS LAIN
+
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 import App from './App.jsx'
-import './index.css' // CSS kustom Anda
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
